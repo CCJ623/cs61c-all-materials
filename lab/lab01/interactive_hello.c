9b8975c4b8386c157a1e482583adfc20dca81064
Change Chan <1974617416@qq.com>
2024-07-12 21:45:00 +0800

    finish lab01

--- lab/lab01/interactive_hello.c
+++ lab/lab01/interactive_hello.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+#include <string.h>
 
 #define MAX_LEN 80
 
 int main(int argc, char *argv[]) {
     char a_word[MAX_LEN];
-
+    strcpy(a_word, argv[1]);
     printf("What's your name?\n");
-    fgets(a_word, MAX_LEN, stdin);
+    //fgets(a_word, MAX_LEN, stdin);
     printf("Hey, %sI just really wanted to say hello to you.\nI hope you have a wonderful day.", a_word);
 
     return 0;
--- lab/lab01/ll_cycle.c
+++ lab/lab01/ll_cycle.c
@@ -3,5 +3,15 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
+    node* tortoise = head, *hare = head;
+    while (1){
+	if (hare->next == NULL || hare->next->next == NULL)
+		return 0;
+	if (hare->next == tortoise || hare->next->next == tortoise)
+		return 1;
+	hare = hare->next->next;
+	tortoise = tortoise->next;
+		
+    }
     return 0;
 }
