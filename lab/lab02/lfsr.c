ef81042ae1c4ff09d9fceaa9e839844c6027b661
Change Chan <1974617416@qq.com>
2024-07-16 12:18:18 +0800

    Finish lab02

--- lab/lab02/lfsr.c
+++ lab/lab02/lfsr.c
@@ -5,6 +5,10 @@
 #include "lfsr.h"
 
 void lfsr_calculate(uint16_t *reg) {
-    /* YOUR CODE HERE */
+  /* YOUR CODE HERE */
+  uint16_t shift_in = (*reg) & 1;
+  shift_in ^= (*reg >> 2) & 1;
+  shift_in ^= (*reg >> 3) & 1;
+  shift_in ^= (*reg >> 5) & 1;
+  *reg = (*reg >> 1) + (shift_in << 15);
 }
-
